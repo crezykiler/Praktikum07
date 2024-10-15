9d85ac68b1888dfa075645f3988e411495338553
zaki setyawan <kiminsetyawan@gmail.com>
2024-10-16 00:00:12 +0700

    sudah modif

--- SiakadFor17.java
+++ SiakadFor17.java
@@ -4,22 +4,30 @@ public class SiakadFor17 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-    
+
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0, batas_lulus = 60; 
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
+            nilai = sc.nextDouble();
 
-    for (int i = 1; i <= 10; i++) {
-        System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
-        nilai = sc.nextDouble();
-        if(nilai > tertinggi) {
-            tertinggi = nilai;
-        }
-        if(nilai < terendah) {
-            terendah = nilai;
-        }
-    }
+            if(nilai > tertinggi) {
+                tertinggi = nilai;
+            }
+            if(nilai < terendah) {
+                terendah = nilai;
+            }
 
-    System.out.println("Nilai Tertinggi " + tertinggi);
-    System.out.println("Nilai Terendah " + terendah);
+            if(nilai >= batas_lulus) {
+                lulus++;
+            } else {
+                tidakLulus++;
+            }
+        }
+
+        System.out.println("Nilai Tertinggi: " + tertinggi);
+        System.out.println("Nilai Terendah: " + terendah);
+        System.out.println("Jumlah Mahasiswa Lulus: " + lulus);
+        System.out.println("Jumlah Mahasiswa Tidak Lulus: " + tidakLulus);
     }
-    
 }
